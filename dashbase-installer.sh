e9d61d7662b74fd7961f25f3d10e5bde9cebfbb0
Chao <oleooo@126.com>
2019-12-09 18:08:56 +0800

    remove useless files

--- dashbase-installer.sh
+++ dashbase-installer.sh
@@ -12,7 +12,7 @@ kubectl create clusterrolebinding admin-user-binding --clusterrole=cluster-admin
 curl -k https://dashbase-public.s3-us-west-1.amazonaws.com/admindash-sts.yaml -o admindash-sts.yaml
 kubectl apply -f admindash-sts.yaml -n dashbase
 
-# git
+# kubectl wait --for=condition=available sts/admindash -n dashbase
 sleep 100
 
 # create tiller service account in kube-system namespace
