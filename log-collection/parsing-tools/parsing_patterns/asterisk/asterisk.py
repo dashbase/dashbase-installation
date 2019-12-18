e361380abad952764aac1b96de59816b6bc6ceac
pushkar-dashbase <pushkar@dashbase.io>
2019-12-18 22:30:23 +0530

    Read location of logger.conf Read location of yml config to update TODO: Code cleanup and downgrade python version i.e. support python 2.7

--- log-collection/parsing-tools/parsing_patterns/asterisk/asterisk.py
+++ log-collection/parsing-tools/parsing_patterns/asterisk/asterisk.py
@@ -2,6 +2,9 @@
 import os
 import sys
 import locale
+import logging
+import yaml
+
 
 if sys.version > '3':
     import configparser
@@ -66,7 +69,7 @@ def translate_command(buffer, pattern, index, old_inside):
     else:
         command = PATTERNS.get("%" + first_char, None)
         if not command:
-            print("Found unsupported specifications: %{}".format(first_char))
+            logging.info("Found unsupported specifications: %{}".format(first_char))
             exit(1)
         else:
             if old_inside:
@@ -152,12 +155,12 @@ def get_pattern(section):
 
 
 def read_config_file(filename):
-    print("Reading file: {}".format(filename))
+    logging.info("Reading file: {}".format(filename))
 
-    buffer = ''
+    buffer = ''  # type: str
 
     if not os.path.isfile(filename):
-        print("File {} is not found".format(filename, flush=True))
+        logging.info("File {} is not found".format(filename, flush=True))
         return buffer
 
     with open(filename, 'r') as f:
@@ -173,13 +176,53 @@ def read_config_file(filename):
                 buffer += read_config_file(include_filename)
 
 
+dict_list = []
+
+
+def writeConfig(pattern):
+    conf_file_path = input("Enter templates configs path (press enter to create config in preset working directory): ")
+    if(len(conf_file_path) < 1):
+        conf_file_path = "asterisk.yaml"
+    package_dir = os.path.dirname(os.path.abspath(__file__))
+    patternConfig = os.path.join(package_dir, conf_file_path)
+    #TODO: add muti-line support
+    dict_file = {"pattern": pattern, "type": 'grok'}
+
+    if(len(conf_file_path) < 1):
+        with open(patternConfig, "a+") as fp:
+            try:
+                fp.truncate(0)
+                fp.write("\n")
+                yaml.dump(dict_file, fp)
+            except:
+                logging.error("Failed to dump YAML config.")
+            finally:
+                fp.close()
+    else:
+        with open(conf_file_path, "w") as fp:
+            config = yaml.load(fp)
+            #TODO: Review this.
+            logging.debug(config[0]['fields']['_message_parser']['parsers']['grok']['parsers']['asterisk']['pattern'])
+            try:
+                config[0]['fields']['_message_parser']['parsers']['grok']['parsers']['asterisk']['pattern'] = pattern
+                yaml.dump(config, fp)
+            except:
+                logging.error("Failed to dump YAML config.")
+            finally:
+                fp.close()
+
+
+
+
 if __name__ == '__main__':
+
     if locale.getdefaultlocale()[0] != 'en_US':
-        print("This machine is not in the locale of 'en_US'. This may break the dashbase parsing because Asterisk "
+        logging.info("This machine is not in the locale of 'en_US'. This may break the dashbase parsing because Asterisk "
               "will output log according to the current locale")
 
-    # TODO input config file path
-    buffer = read_config_file('../logger.conf')
+
+    conf_file_path = input("Enter asterisk logger.conf path : ")
+    buffer = read_config_file(conf_file_path)
 
     try:
         config = configparser.ConfigParser(interpolation=configparser.Interpolation())
@@ -187,12 +230,13 @@ if __name__ == '__main__':
         config.read_string(buffer)
 
         if 'general' not in config:
-            print("Section 'general' is not found in the config files. "
+            logging.info("Section 'general' is not found in the config files. "
                   "These config files are considered as broken ones.")
             exit(1)
 
         pattern = config['general'].get('dateformat')
 
-        print("Your pattern is: '{}'".format(get_pattern(config['general'])))
+        logging.info("Your pattern is: '{}'".format(get_pattern(config['general'])))
+        writeConfig(get_pattern(config['general']))
     except configparser.ParsingError as e:
-        print(e)
+        logging.error(e)
