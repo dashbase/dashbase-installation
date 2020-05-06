1afbb0147e54f1744afb1ae78a0b248320ebf547
Ray Lui <rlui@dashbase.io>
2020-05-06 10:09:02 -0700

    changed table name freeswitch to LOGS  as variable in exporter_metric.yaml file

--- deployment-tools/dashbase-installer-tinysetup.sh
+++ deployment-tools/dashbase-installer-tinysetup.sh
@@ -557,6 +557,8 @@ update_dashbase_valuefile() {
   # update table name
   log_info "update dashbase-values.yaml file with table name = $TABLENAME"
   kubectl exec -it admindash-0 -n dashbase -- sed -i "s|LOGS|$TABLENAME|" /data/dashbase-values.yaml
+  kubectl exec -it admindash-0 -n dashbase -- sed -i "s|LOGS|$TABLENAME|" /data/exporter_metric.yaml
+
   # update ucaas feature
   if [ "$UCAAS_FLAG" == "true" ]; then
     log_info "update dashbase-values.yaml file to enable UCAAS features"
