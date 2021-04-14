858efba894fa2c825fd31ec7bcc55f2444d9db4c
Cuichen Li <cuichen.li@appdynamics.com>
2021-04-14 16:20:50 +0800

    Use existing keytool to do the whole thing

--- deployment-tools/dashbase-admin/dashbase_setup_tarball/https_dashbase.sh
+++ deployment-tools/dashbase-admin/dashbase_setup_tarball/https_dashbase.sh
@@ -14,10 +14,6 @@ else
    NAMESPACE=$1
 fi
 
-
-KAFKA_CERT_FILE="./kafka-ca-cert"
-KAFKA_KEY_FILE="./kafka-ca-key"
-
 # remove previous dashbase cert, key and keystore in the current folder
 [ -e dashbase-keystore ] && rm -rf dashbase-keystore
 [ -e dashbase-keystore.p12 ] && rm -rf dashbase-keystore.p12
@@ -25,14 +21,7 @@ KAFKA_KEY_FILE="./kafka-ca-key"
 [ -e dashbase-key.pem ] && rm -rf dashbase-key.pem
 [ -e dashbase_keystore_password ] && rm -rf dashbase_keystore_password
 [ -e https.yaml ] && rm -rf https.yaml
-[ -e "${KAFKA_CERT_FILE}" ] && rm -rf "${KAFKA_CERT_FILE}"
-[ -e "${KAFKA_KEY_FILE}" ] && rm -rf "${KAFKA_KEY_FILE}"
-[ -e kafka.client.pkcs1.key ] && rm -rf kafka.client.pkcs1.key
-[ -e kafka.client.key ] && rm -rf kafka.client.key
-[ -e kafka.client.csr ] && rm -rf kafka.client.csr
-[ -e kafka.client.pem ] && rm -rf kafka.client.pem
-[ -e kafka-cert-signed.pem ] && rm -rf kafka-cert-signed.pem
-[ -e kafka.csr ] && rm -rf kafka.csr
+[ -e dashbase-ca.crt ] && rm -rf dashbase-ca.crt
 
 # bash generate random 32 character alphanumeric string (upper and lowercase) and
 
@@ -43,9 +32,6 @@ KEYSTORE_PASS=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 #echo "entered namespace is $NAMESPACE"
 echo $KEYSTORE_PASS > dashbase_keystore_password
 KEYSTORE_PASSWORD=$(cat dashbase_keystore_password)
-
-echo "Generate CA Certificate"
-openssl req -nodes -new -x509 -keyout "${KAFKA_KEY_FILE}" -out "${KAFKA_CERT_FILE}" -days 3650 -subj "/CN=kafka/O=Dashbase"
 
 echo "Creating dashbase-keystore file"
 
@@ -70,17 +56,7 @@ openssl pkcs12 -in dashbase-keystore.p12 -nokeys -out dashbase-cert.pem -passin 
 openssl pkcs12 -in dashbase-keystore.p12 -nodes -nocerts -out dashbase-key.pem -passin pass:$KEYSTORE_PASSWORD
 
 
-echo "Importing CARoot to Kafka keystore"
-keytool -keystore dashbase-keystore -alias CARoot -importcert -file "${KAFKA_CERT_FILE}" -noprompt -storepass "${KEYSTORE_PASSWORD}"
-
-echo "Sign kafka certificate and import it"
-keytool -keystore dashbase-keystore -alias dashbase -certreq -file kafka.csr -storepass "$KEYSTORE_PASSWORD"
-openssl x509 -req -CA "${KAFKA_CERT_FILE}" -CAkey "${KAFKA_KEY_FILE}" -in kafka.csr -out kafka-cert-signed.pem -days 3650 -CAcreateserial
-keytool -keystore dashbase-keystore -alias kafka -import -file kafka-cert-signed.pem -noprompt -storepass "$KEYSTORE_PASSWORD"
-
-echo "Generate kafka client key"
-openssl req -new -sha256 -key dashbase-key.pem -subj "/CN=kafka-client/O=Dashbase" -out kafka.client.csr
-openssl x509 -req -CA "${KAFKA_CERT_FILE}" -CAkey "${KAFKA_KEY_FILE}" -in kafka.client.csr -out kafka.client.pem -days 3650 -CAcreateserial
+keytool -export -storepass "${KEYSTORE_PASSWORD}" -alias dashbase -keystore ./dashbase-keystore -file dashbase-ca.crt -rfc
 
 echo "signed signed-cert generation for dashbase is completed"
 echo "you should have the following files:"
@@ -88,6 +64,7 @@ echo "1. dashbase-kestore  java keystore for dashbase"
 echo "2. dashbase-keystore.p12 P12 format file for dashbase-keystore"
 echo "3. dashbase-cert.pem base 64 cert file for dashbase"
 echo "4. dashbase-key.pem  base 64 key file for dashbase"
+echo "5. dashbase-ca.crt   base64 CA file for dashbase"
 echo "The CN of this self-signed cert is dashbase.io"
 
 # create Base 64 encryption for generated key, cert, keystore, keystore password
@@ -98,16 +75,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
    DASHBASE_KEYSTORE_B64=`cat dashbase-keystore |base64`
    DASHBASE_CERT_B64=`cat dashbase-cert.pem |base64`
    DASHBASE_KEY_B64=`cat dashbase-key.pem |base64`
-   KAFKA_CA_CERT=$(base64 < "${KAFKA_CERT_FILE}")
-   KAFKA_CLIENT_CERT=$(base64 < kafka.client.pem)
+   DASHBASE_CA_B64=$(base64 < "dashbase-ca.crt")
 elif [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-musl" ]]; then
    echo "create dashbase Base 64 encryption for generated key, cert, keystore, keystore password from linux workstation"
    DASHBASE_KEYSTORE_PASS_B64=`echo -n "$KEYSTORE_PASSWORD" |base64 -w 0`
    DASHBASE_KEYSTORE_B64=`cat dashbase-keystore |base64 -w 0`
    DASHBASE_CERT_B64=`cat dashbase-cert.pem |base64 -w 0`
    DASHBASE_KEY_B64=`cat dashbase-key.pem |base64 -w 0`
-   KAFKA_CA_CERT=$(base64 -w 0 < "${KAFKA_CERT_FILE}")
-   KAFKA_CLIENT_CERT=$(base64 -w 0 < kafka.client.pem)
+   DASHBASE_CA_B64=$(base64 -w 0 < "dashbase-ca.crt")
 else
    echo "OSTYPE is not supported"
    exit
@@ -129,7 +104,7 @@ fi
 # feed the base64 outputs of key, cert, keystore, and keystore password into https-dashbase.yaml file
 
 echo "feed the base64 outputs of key, cert, keystore, and keystore password into https-dashbase.yaml file"
-cp https-dashbase-template.yaml https-dashbase.yaml
+cp ./deployment-tools/dashbase-admin/dashbase_setup_tarball/https-dashbase-template.yaml https-dashbase.yaml
 
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -137,17 +112,13 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
    sed -i .bak "s|KEYPASS|${DASHBASE_KEYSTORE_PASS_B64}|" https-dashbase.yaml
    sed -i .bak "s|CERTPEM|${DASHBASE_CERT_B64}|" https-dashbase.yaml
    sed -i .bak "s|KEYPEM|${DASHBASE_KEY_B64}|" https-dashbase.yaml
-   sed -i .bak "s|KAFKA_CA_CERT|${KAFKA_CA_CERT}|" https-dashbase.yaml
-   sed -i .bak "s|KAFKA_CLIENT_CERT|${KAFKA_CLIENT_CERT}|" https-dashbase.yaml
-   sed -i .bak "s|KAFKA_CLIENT_PEM|${DASHBASE_KEY_B64}|" https-dashbase.yaml
+   sed -i .bak "s|DASHBASE_CA|${DASHBASE_CA_B64}|" https-dashbase.yaml
 elif [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-musl" ]]; then
    sed -i "s|KEYSTORE|${DASHBASE_KEYSTORE_B64}|" https-dashbase.yaml
    sed -i "s|KEYPASS|${DASHBASE_KEYSTORE_PASS_B64}|" https-dashbase.yaml
    sed -i "s|CERTPEM|${DASHBASE_CERT_B64}|" https-dashbase.yaml
    sed -i "s|KEYPEM|${DASHBASE_KEY_B64}|" https-dashbase.yaml
-   sed -i "s|KAFKA_CA_CERT|${KAFKA_CA_CERT}|" https-dashbase.yaml
-   sed -i "s|KAFKA_CLIENT_CERT|${KAFKA_CLIENT_CERT}|" https-dashbase.yaml
-   sed -i "s|KAFKA_CLIENT_PEM|${DASHBASE_KEY_B64}|" https-dashbase.yaml
+   sed -i "s|DASHBASE_CA|${DASHBASE_CA_B64}|" https-dashbase.yaml
 else
    echo "OSTYPE is not supported"
    exit
